93950d549108f78719c7fc30866cad6039b90599
HubCreator <chocomilk8604@gmail.com>
2022-12-03 13:48:51 +0900

    fix(ErrorMessage) : 오류 메시지의 앞에 [ERROR]를 붙이도록

--- src/main/java/vendingmachine/enums/ErrorMessage.java
+++ src/main/java/vendingmachine/enums/ErrorMessage.java
@@ -3,10 +3,11 @@ package vendingmachine.enums;
 public enum ErrorMessage {
     INVALID_RANGE_PRICE("올바르지 않은 금액이 입력되었습니다.");
 
+    private static final String errorHead = "[ERROR] ";
     private final String message;
 
     ErrorMessage(String message) {
-        this.message = message;
+        this.message = errorHead + message;
     }
 
     public String getMessage() {
