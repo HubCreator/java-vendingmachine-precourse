7c85518e46e378ca100dc4e9f31faab80cc61525
HubCreator <chocomilk8604@gmail.com>
2022-12-13 14:17:53 +0900

    test(ItemTest) : 테스트 케이스 추가

--- src/test/java/vendingmachine/domain/items/ItemTest.java
+++ src/test/java/vendingmachine/domain/items/ItemTest.java
@@ -1,15 +1,44 @@
 package vendingmachine.domain.items;
 
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class ItemTest {
 
     @Test
-    void name() {
-        Item 콜라1 = new Item("콜라");
-        Item 콜라2 = new Item("콜라");
-        assertThat(콜라1).isEqualTo(콜라2);
+    void 기본_객체_테스트() {
+        assertThat(new Item("콜라")).isEqualTo(new Item("콜라"));
+    }
+
+    @Nested
+    class 올바르지_않은_상품_가격 {
+
+        @ParameterizedTest
+        @ValueSource(strings = {"", " ", "!!"})
+        void 문자열_입력(String value) {
+            assertThatThrownBy(() -> new Item("콜라", value))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @ParameterizedTest
+        @ValueSource(strings = {"10", "90", "101", "301"})
+        void 올바르지_않은_입력_범위(String value) {
+            assertThatThrownBy(() -> new Item("콜라", value))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"100", "110", "300"})
+    void 올바른_상품가격_입력(String value) {
+        assertThatCode(() -> new Item("콜라", value))
+                .doesNotThrowAnyException();
     }
 }
